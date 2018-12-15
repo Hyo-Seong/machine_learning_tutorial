52dbac66e825f38b795e9fdff9dbf3a9a5281907
Hyo-Seong <dlgytjd0907@naver.com>
2018-12-16 04:35:22 +0900

    ch01. BeautifulSoup 기본 사용법

---
+++ 01-크롤링과_스크레이핑/2.BeautifulSoup로_스크레이핑하기/bs_test1.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+html = """
+<html><body>
+  <h1>스크레이핑이란?</h1>
+  <p>웹 페이지를 분석하는 것</p>
+  <p>원하는 부분을 추출하는 것</p>
+</body></html>
+"""
+
+soup = BeautifulSoup(html, 'html.parser')
+
+h1 = soup.html.body.h2
+p1 = soup.html.body.p
+p2 = p1.next_sibling.next_sibling
+
+# 존재하지 않는 태그를 입력시 print할때 오류 발생 ex) 
+# h2 = soup.html.body.h2
+# print("h2 = " + h2.string)
+# AttributeError: 'NoneType' object has no attribute 'string' 
+# NoneType을 체크하려면 다음 코드를 사용하자.
+# if variable is None: 
+# 참고링크 : https://stackoverflow.com/questions/23086383/how-to-test-nonetype-in-python
+
+print("h1 = " + h1.string)
+print("p1 = " + p1.string)
+print("p2 = " + p2.string)
+
+# hi.string 대신 h1을 하면 => TypeError: Can't convert 'Tag' object to str implicitly 발생!
