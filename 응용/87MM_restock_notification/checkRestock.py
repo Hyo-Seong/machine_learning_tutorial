76063d109526b5076bd9890996282a2e1c94bb06
Hyo-Seong <dlgytjd0907@naver.com>
2019-01-01 23:04:18 +0900

    옵션 스크래핑 완성
    다듬자

--- 응용/87MM_restock_notification/checkRestock.py
+++ 응용/87MM_restock_notification/checkRestock.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
-
+from urllib.request import urlopen, Request
 
 def checkRestock(scrapingUrl):
-    print(scrapingUrl)
+    req = Request(scrapingUrl)
+    html = urlopen(req).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.select("#option > tr > td > select > option")[2].string)
 
 if __name__ == "__main__":
     print("확인하려는 87MM의 링크를 입력하세요 >> ", end='')
     scrapingUrl = input()
 
+    print("\n사이즈를 입력하세요(S,M,L) >> ", end='')
+    scrapingUrl = input()
     checkRestock(scrapingUrl)
