0da5b747592a919d4eac6464e14133124856cbc7
Hyo-Seong <dlgytjd0907@naver.com>
2019-01-02 22:28:26 +0900

    checksizelist
    근데 옆에 [품절] 이 캐치가 안된다,,

--- 응용/87MM_restock_notification/checkRestock.py
+++ 응용/87MM_restock_notification/checkRestock.py
@@ -1,16 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
-def checkRestock(scrapingUrl):
+def getHtml(scrapingUrl):
+    req = Request(scrapingUrl)
+    html = urlopen(req).read()
+    return BeautifulSoup(html, 'html.parser')
+
+def checkSizeList(scrapingUrl):
+    soup = getHtml(scrapingUrl)
+    list = soup.findAll('option')
+    for l in list:
+        print(l.string)
+
+def checkRestock(scrapingUrl, size):
     req = Request(scrapingUrl)
     html = urlopen(req).read()
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.select("#option > tr > td > select > option")[2].string)
+    print(soup.select("#option > tr > td > select > option >text")[2])
 
 if __name__ == "__main__":
     print("확인하려는 87MM의 링크를 입력하세요 >> ", end='')
     scrapingUrl = input()
 
-    print("\n사이즈를 입력하세요(S,M,L) >> ", end='')
-    scrapingUrl = input()
-    checkRestock(scrapingUrl)
+    checkSizeList(scrapingUrl)
+    # print("\n사이즈를 입력하세요(S,M,L) >> ", end='')
+    # size = input()
+    # checkRestock(scrapingUrl)
