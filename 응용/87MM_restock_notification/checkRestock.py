8dc88988aadd5dbd93f4177cd5615c0cf94cdc71
Hyo-Seong <dlgytjd0907@naver.com>
2019-01-07 19:58:54 +0900

    함수화

--- 응용/87MM_restock_notification/checkRestock.py
+++ 응용/87MM_restock_notification/checkRestock.py
@@ -3,17 +3,17 @@ from urllib.request import urlopen, Request
 from selenium import webdriver
 
 
-def getHtml(scrapingUrl):
-
-    # 웹드라이버 실행시 크롬창 실행되는것 숨기는 코드
+def initWebDriver():
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
 
     # 설정한 옵션을 웹드라이버에 적용한다.
+    global driver
     driver = webdriver.Chrome('C:/Users/hyoseong/Downloads/chromedriver_win32/chromedriver.exe', chrome_options=options)
 
+def getHtml(scrapingUrl):
     driver.get(scrapingUrl)
     # 전체 페이지 코드를 불러온다. 코드를 가지고 있다가 가공은 다른 곳에서 진행된다.
     html = driver.page_source
@@ -26,7 +26,19 @@ def checkSizeList(scrapingUrl):
     sizeList = []
     for i in range(2, len(list)):
         sizeList.append(list[i]['value'])
-    return sizeList
+    if len(sizeList) == 0:
+        print('사이즈정보를 받아들이지 못하였습니다.')
+        getLink()
+    chooseSize(sizeList)
+
+def aaa(sizeList):
+    print('aa')
+
+def getLink():
+    print("확인하려는 87MM의 링크를 입력하세요 >> ", end='')
+    scrapingUrl = input()
+
+    checkSizeList(scrapingUrl)
 
 def checkRestock(scrapingUrl, size):
     req = Request(scrapingUrl)
@@ -34,14 +46,16 @@ def checkRestock(scrapingUrl, size):
     soup = BeautifulSoup(html, 'html.parser')
     print(soup.select("#option > tr > td > select > option >text")[2])
 
-if __name__ == "__main__":
-    print("확인하려는 87MM의 링크를 입력하세요 >> ", end='')
-    scrapingUrl = input()
-
-    sizeList = checkSizeList(scrapingUrl)
-    print("\n사이즈를 입력하세요 ")
+def chooseSize(sizeList):
     for idx, val in enumerate(sizeList):
         print(idx + 1, "\b.", val)
-    print("번호를 입력하세요. >> ", end='')
+    print("원하는 사이즈의 번호를 입력하세요. >> ", end='')
     size = input()
+
+if __name__ == "__main__":
+    initWebDriver()
+
+    getLink()
+
+    
     # checkRestock(scrapingUrl, size)
