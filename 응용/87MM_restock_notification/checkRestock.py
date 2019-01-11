238cdd5c0a41c45462502c80b6060d04ed4f0be7
Hyo-Seong <dlgytjd0907@naver.com>
2019-01-11 23:50:40 +0900

    인자로 전달되던 scrapingUrl를 global로 변경

--- 응용/87MM_restock_notification/checkRestock.py
+++ 응용/87MM_restock_notification/checkRestock.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen, Request
 from selenium import webdriver
 import os
     
+
+
 def cls(): 
     os.system('CLS')
 
@@ -18,16 +20,16 @@ def initWebDriver():
     driver = webdriver.Chrome('C:/Users/hyoseong/Downloads/chromedriver_win32/chromedriver.exe', chrome_options=options)
     cls()
 
-def getHtml(scrapingUrl):
+def getHtml():
     driver.get(scrapingUrl)
     # 전체 페이지 코드를 불러온다. 코드를 가지고 있다가 가공은 다른 곳에서 진행된다.
     html = driver.page_source
     return BeautifulSoup(html, 'html.parser')
 
 
-def checkSizeList(scrapingUrl):
-    soup = getHtml(scrapingUrl)
-    list = soup.findAll('option') # TODO - HyoSeong 이부분 수정해야함. option으로 찾으면 다른값 섞일수 있음.
+def checkSizeList():
+    soup = getHtml()
+    list = soup.findAll('option') # TODO - HyoSeong 이부분 수정해야함. option으로 찾으면 다른값 섞일 수 있음.
     
     sizeList = []
     
@@ -40,16 +42,14 @@ def checkSizeList(scrapingUrl):
 
     chooseSize(sizeList)
 
-def aaa(sizeList):
-    print('aa')
-
 def getLink():
     print("확인하려는 87MM의 링크를 입력하세요 >> ", end='')
+    global scrapingUrl
     scrapingUrl = input()
 
-    checkSizeList(scrapingUrl)
+    checkSizeList()
 
-def checkRestock(scrapingUrl, size):
+def checkRestock(size):
     req = Request(scrapingUrl)
     html = urlopen(req).read()
     soup = BeautifulSoup(html, 'html.parser')
@@ -65,11 +65,10 @@ def chooseSize(sizeList):
         cls()
         chooseSize(sizeList)
 
+    # checkRestock(sizeList[int(size) -1])
+
 
 if __name__ == "__main__":
     initWebDriver()
 
     getLink()
-
-    
-    # checkRestock(scrapingUrl, size)
